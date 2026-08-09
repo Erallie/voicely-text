b56ff43e561c4893b635048963b4f2fd08adcf18
Erallie <eds.gozar@outlook.com>
2026-08-09 00:03:46 -0700

    Made it only read translated messages

--- voicely-text.py
+++ voicely-text.py
@@ -845,7 +845,10 @@ async def process_translation_message(message: discord.Message):
 
 @bot.event
 async def on_message(message: discord.Message):
-    if message.author.id == TRANSLATION_BOT_ID:
+    if (
+        message.author.id == TRANSLATION_BOT_ID
+        and message.content.startswith("🗣️")
+    ):
         await process_translation_message(message)
 
     elif (
